c979dc00339e43354e0e315c8204c55084086ce6
Eric Smith <eric@8thlight.com>
2010-10-16 17:27:58 -0500

    I have a unit test main.

---
+++ UnitTests/unitTestMain.m
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+#import "CubicleWarsAppDelegate.h"
+
+int main(int argc, char *argv[]) 
+{  
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  
+  CubicleWarsAppDelegate *delegate = [[CubicleWarsAppDelegate alloc] init];
+  
+  
+  
+  [pool release];
+  return 0;
+}
